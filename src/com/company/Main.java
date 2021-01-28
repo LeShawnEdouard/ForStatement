7d4f28cfdf90fbc5da2d2c98e924251a681d003b
LeShawn Edouard <leshawnmedouard@gmail.com>
2021-01-27 21:15:22 -0500

    "Created calculateInterest method and pass thru amount and interest arguments."

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+
+        for(int i=0; i<5; i++) {
+            System.out.println("Loop " + i + " hello!");
+        }
     }
 
     public static double calculateInterest(double amount, double interestRate) {
